3a77338a62b0a819324a966e7f0323d06b161619
Gaurav Tripathi <tripathigaurav38@gmail.com>
2024-03-18 15:23:28 +0530

    added net_build project

---
+++ BrandNewMVCWithDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrandNewMVCWithDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
